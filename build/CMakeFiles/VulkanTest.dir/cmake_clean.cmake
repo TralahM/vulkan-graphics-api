8e2f61012acd4152049c7ff862396c3229ed274c
TralahM <musyoki.brian@tralahtek.com>
2020-05-03 05:16:33 +0300

    build works

---
+++ build/CMakeFiles/VulkanTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanTest.dir/VulkanFunctions.cpp.o"
+  "VulkanTest.pdb"
+  "VulkanTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
